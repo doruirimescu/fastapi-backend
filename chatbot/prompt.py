39d3f82dc07b5af942568da6fcabeab818dcb9ca
Doru Irimescu <>
2023-08-22 14:05:30 +0300

    Fix summarizer prompt

--- chatbot/prompt.py
+++ chatbot/prompt.py
@@ -5,13 +5,14 @@ def PROFILER_SYSTEM_PROMPT(schema: str) -> str:
     return f"""
 You are a virtual career assistant tasked with helping job seekers create a comprehensive profile that
 can be used to find suitable job opportunities and generate a tailored cover letter. Your shall ask the user
-step by step questions in order to find the user information. When you have all the required information, you
-shall simply reply with the word "stop" to end the conversation.
+step by step questions in order to find the user information.
 
 The required user information is provided in this json schema:
 
 {schema}
 For enum types, tell the user the possible values and ask them to choose one.
+
+When you have all the required information, you reply with "YOUR PROFILE IS COMPLETE". The user will know what to do.
 """
 
 
@@ -21,8 +22,11 @@ def SUMMARIZER_SYSTEM_PROMPT(schema: str) -> str:
 
 def SUMMARIZER_SYSTEM_PROMPT_2(schema: str) -> str:
     return (
-        f"Extract the following information from the following conversation between a user and a chatbot."
-        f" Information is defined in schema: {schema}"
+        f"You are an information extractor. \n"
+        f"Information is defined in schema: {schema}"
+        f"You shall be presented with a passage of text. \n"
+        f"Extract the following information in json format. \n"
+        f"Discard any incomplete fields. \n"
     )
 
 
@@ -45,6 +49,7 @@ def ORCHESTRATOR_INTRO_PROMPT(does_user_profile_exist: bool) -> str:
     else:
         prompt += "Ask users if they want to start creating a profile."
     return prompt
+
 
 def ACTION_SELECTOR_PROMPT(user_action_enum) -> str:
     return (
