3af8b27b8eb7751d5288b3a00469bcd1541b0e19
Doru Irimescu <>
2023-08-23 12:33:53 +0300

    Add timeout to tests

--- chatbot/test/test_summarizer.py
+++ chatbot/test/test_summarizer.py
@@ -1,40 +1,41 @@
 import unittest
 from chatbot.test.data import valid_data
-from chatbot.test.incomplete_education import incomplete_education, incomplete_education_json
-from chatbot.test.incomplete_work_experience import incomplete_work_experience, incomplete_work_experience_json
-from chatbot.test.invalid_desired_job_type import invliad_desired_job_type, invliad_desired_job_type_json
+from chatbot.test.incomplete_education import incomplete_education
+from chatbot.test.incomplete_work_experience import incomplete_work_experience
+from chatbot.test.invalid_desired_job_type import invliad_desired_job_type
 
 from chatbot.summarizer import Summarizer
 from chatbot.user_model import JobSeekerProfile, SCHEMA
+import pytest
 from dotenv import load_dotenv
 load_dotenv()
-import pydantic
 
+#TODO: dump generated JobSeekerProfile into jsons, and use them to test the summarizer
 class TestSummarizer(unittest.TestCase):
+    @pytest.mark.timeout(60)
     def test_summarize_valid_data(self):
         s = Summarizer(SCHEMA)
         result = s.reply(valid_data)
         self.assertIsInstance(result, dict)
         JobSeekerProfile(**result)
 
+    @pytest.mark.timeout(60)
     def test_summarize_incomplete_education(self):
         s = Summarizer(SCHEMA)
         result = s.reply(incomplete_education)
-        # self.assertEqual(incomplete_education_json, result)
 
         JobSeekerProfile(**result)
 
+    @pytest.mark.timeout(60)
     def test_summarize_incomplete_work_experience(self):
         s = Summarizer(SCHEMA)
         result = s.reply(incomplete_work_experience)
-        # self.assertEqual(incomplete_work_experience_json, result)
-        # Generated jsons differ sometimes from call to call, so we can't compare them
 
         JobSeekerProfile(**result)
 
+    @pytest.mark.timeout(60)
     def test_summarize_invalid_desired_job_type(self):
         s = Summarizer(SCHEMA)
         result = s.reply(invliad_desired_job_type)
-        # self.assertEqual(invliad_desired_job_type_json, result)
 
         JobSeekerProfile(**result)
